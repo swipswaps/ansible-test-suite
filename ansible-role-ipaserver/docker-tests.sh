38cdba9adcbb37e7f03ddc806e816c888503d371
Abhinav Y <abhinav@MacBook-Pro.local>
2017-02-19 16:24:04 +0530

    removing test 1.9.6 ansible
    ansible-role-ipaservers uses block which is not supported by 1.9.6

--- ansible-role-ipaserver/docker-tests.sh
+++ ansible-role-ipaserver/docker-tests.sh
@@ -31,7 +31,7 @@ init="/sbin/init"
 run_opts=("--privileged")
 
 # Supported versions of ansible stable releases
-readonly ansible_versions=(1.9.6.0 2.0.0.0 2.1.0.0 2.2.0.0 ) 
+readonly ansible_versions=(2.0.0.0 2.1.0.0 2.2.0.0 ) 
 #latest #once block issue fixed with 2.2.1
 ansible_version=latest 
 
